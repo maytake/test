5756ec36df5c1d495affe297ab789c76b220cd98
LongKing <LongKing@longKingdeMacBook-Pro.local>
2019-01-10 22:53:04 +0800

    hello mundo change

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-
-const arr=[1,2,3];
-const all= ()=>{
-	arr.reduce((prev, next)=>{
+const arr = [1, 2, 3];
+const all = () => {
+	arr.reduce((prev, next) => {
 		return prev；
 	})
 }
- const a=()=>{
- 	Number('32');
- }
+const a = () => {
+	Number('32');
+}
+const b = () => {
+	Number('32');
+}
