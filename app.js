934836c743191956cdb11c457c98c651a78deeda
LongQing <1042689488@qq.com>
2019-01-11 13:31:20 +0800

    c

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const arr = [1, 2, 3];
-const allchange = () => {
-	arr.reduce((prev, next) => {
-		return prev；
-	})
-}
-const a = () => {
-	Number('32');
-}
-const b = () => {
-	Number('32');
-}
-const release = () => {
-	Number('32');
-}
-
-const newA = () => {
-	Number('32');
+const c = () => {
+	Number('cccccccc');
 }
