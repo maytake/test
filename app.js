630e5e76934ace20ea80b7f130e42da09fcf329f
LongQing <1042689488@qq.com>
2019-01-11 13:39:18 +0800

    bbb

--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const arr = [1, 2, 3];
-const allfdsafchange = () => {
-	arr.reduce((prev, next) => {
-		return prev；
-	})
-}
-const dsfdasf = () => {
-	Number('32');
-}
 const b = () => {
-	Number('32');
-}fdasf
-const release fd= () => {
-	Number('3afdas2');
-}
-f
-const newfdsfdasfAc = () => {
-	Number('32');
+	Number('bbbbbb');
 }
